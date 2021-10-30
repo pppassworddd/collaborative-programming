7d9e1c25c945fa04a4b0327f71c133f9be5c94b1
fenriliuguang <1415854799@qq.com>
2021-10-30 10:03:03 +0800

    feat: 框架

---
+++ inc/Judge.h
@@ -0,0 +1,8 @@
+#ifndef _USERIO_H_
+#define _USERIO_H_
+#include<iostream>
+using namespace std;
+
+bool Judge(double res, double userRes);
+
+#endif
---
+++ inc/UserIO.h
@@ -0,0 +1,14 @@
+#ifndef _USERIO_H_
+#define _USERIO_H_
+#include<iostream>
+using namespace std;
+
+int HandleInputGrade(string grade);  // 返回年级
+
+int HandleInputNum(int num);  // 输入题目数量
+
+int HandleInputAnswer(int num);  // 用户输入答案
+
+void HandleOutput();
+
+#endif
