7177066d253a46c84b20a8f39005d9b128679772
pppassworddd <90122078+pppassworddd@users.noreply.github.com>
2021-10-30 10:04:37 +0800

    Update func.c

--- inc/func.c
+++ inc/func.c
@@ -41,3 +41,23 @@ bool Judge(double res, double userRes) {
         return 0;
     }
 }	
+
+
+
+double Level_56(){
+    double num1;
+	int leng;
+	int i=0;
+	num1=quesdata.num[0];
+	while(i=0;i+2<quesdata.Strnum;i++)
+	switch(quesdata.num[i+1]){
+		case 1: num1+=quesdata.num[i+2];break;
+		case 2: num1-=quesdata.num[i+2];break;
+		case 3: num1*=quesdata.num[i+2];break;
+		case 4: num1/=quesdata.num[i+2];break;	
+	}
+	return num1;
+}
+
+
+
