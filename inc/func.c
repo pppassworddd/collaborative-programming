ae30f456dcb7cddf7e537b676e80eaf66ccc2f24
pppassworddd <90122078+pppassworddd@users.noreply.github.com>
2021-10-30 09:57:52 +0800

    Update func.c
    level_56

--- inc/func.c
+++ inc/func.c
@@ -1,2 +1,16 @@
 #include "func.h"
 
+double Level_56(){
+    double num1;
+	int leng;
+	int i=0;
+	num1=quesdata.num[0];
+	while(i=0;i+2<quesdata.Strnum;i++)
+	switch(quesdata.num[i+1]){
+		case 1: num1+=quesdata.num[i+2];breaak;
+		case 2: num1-=quesdata.num[i+2];breaak;
+		case 3: num1*=quesdata.num[i+2];breaak;
+		case 4: num1/=quesdata.num[i+2];breaak;	
+	}
+	return num1;
+}
